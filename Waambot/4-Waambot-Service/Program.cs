19d44f97ad2497200e2a5c35582b5ee719ee87c8
Andrew.Mille5a9 <Andrew.Mille5a9@outlook.com>
2020-03-02 21:55:00 -0500

    added discord.net package with base code in service proj, and blank appsettings for token plopping

--- Waambot/4-Waambot-Service/Program.cs
+++ Waambot/4-Waambot-Service/Program.cs
@@ -1,12 +1,33 @@
-﻿using System;
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Threading.Tasks;
 
 namespace Waambot_Service
 {
     class Program
     {
+        // adapted from https://docs.stillu.cc/guides/getting_started/first-bot.html
         static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
         {
-            Console.WriteLine("Hello World!");
+            var client = new DiscordSocketClient();
+            client.Log += Log;
+            var token = "token";
+
+            await client.LoginAsync(TokenType.Bot, token);
+            await client.StartAsync();
+
+            // wait forever so bot doesn't close itself
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
         }
     }
 }
