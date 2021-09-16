25ad5a3282002eaeb7ce6b96554b9776436d09ad
mille5a9 <andrewm192@outlook.com>
2021-09-16 16:14:43 +0000

    direct reddit image embed !reddit

--- Waambot/4-Waambot-Service/Modules/EmbedModule.cs
+++ Waambot/4-Waambot-Service/Modules/EmbedModule.cs
@@ -1,7 +1,11 @@
 using Discord.Commands;
+using AngleSharp;
+using ScrapySharp.Extensions;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
 using System.IO;
 using System.Linq;
@@ -15,6 +19,28 @@ namespace _4_Waambot_Service.Modules
         [Summary("Analyzes a reddit link and provides a sensible embed")]
         public async Task RedditAsync([Summary("reddit hyperlink")] string link)
         {
+            // 0 remove possible <> decorations suppressing embed
+            link = link.Replace(">", string.Empty);
+            link = link.Replace("<", string.Empty);
+
+            // 1 visit reddit thread
+            var config = AngleSharp.Configuration.Default.WithDefaultLoader();
+            var context = BrowsingContext.New(config);
+            var document = await context.OpenAsync(link);
+            var cellSelector = "a[href*=\"i.redd.it\"]";
+            var cells = document.QuerySelectorAll(cellSelector);
+            Console.WriteLine("Line 28");
+            Console.WriteLine(cells.Length);
+
+            // 2 analyze content of reddit post (.jpg, text post, etc)
+            foreach (var node in cells) {
+                Console.WriteLine("Line 33");
+                Console.WriteLine(node.GetAttribute("href"));
+                await this.Context.Channel.SendMessageAsync(node.GetAttribute("href"));
+            }
+
+            // 3 construct sensible embed based on results from step 2
+
         }
     }
 }
